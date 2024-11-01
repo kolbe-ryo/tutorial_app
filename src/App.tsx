c7dcdd7880878df3fc33fe3fe6aa1645365c21fc
ryo-fukahori <ryo_fukahori@waku-2.com>
2024-11-01 21:29:42 +0900

    add useState hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import LinkButton from "./children/link";
 
 function App() {
-  const title: string = "Hello World!";
+  const title: string = "Todo App";
+  const [num, setNum] = useState(0);
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const addNumber = (val: number): number => {
+    return num + val;
+  }
+
+  function addNumber2(val: number): number {
+    return num + val
+  }
+
 
   return (
     <div className="App">
-      <h1>{title}</h1>
-      <Link to='/test'>
-        ボタン
-      </Link>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
     </div>
   );
 }
